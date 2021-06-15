d066066cb4fbfe140f9d1148377189994a9f465e
Dimitris Kamaritis <dk33@hw.ac.uk>
2021-06-15 16:26:46 +0300

    removed /health-check

--- app.js
+++ app.js
@@ -229,10 +229,6 @@ app.get('/update/error', async (req,res) => {
     res.render('error')
 })
 
-app.get('/health-check', (req,res) => {
-    res.send('ok')
-})
-
 // ----- SERVER ------
 
 // Listen for requests on specified port
